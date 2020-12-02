13d13b465c3bcb6cb5e35385993313a504a532d0
haselby <jeffrey.haselby@gmail.com>
2020-12-01 21:22:06 -0700

    Refactored Day7Proto

--- 7/Day7Proto/Day7Proto/Day7Proto.cpp
+++ 7/Day7Proto/Day7Proto/Day7Proto.cpp
@@ -5,41 +5,43 @@
 #include <vector>
 #include <set>
 
-void everyCombination(std::vector<int> orderedPhases, std::set<int> availablePhases, int depth) {
+void printVector(std::vector<int> myVector) {
+    for (auto x : myVector) {
+        std::cout << x;
+    }
+    std::cout << std::endl;
+}
 
+void everyCombination(std::vector<int> orderedPhases, std::set<int> availablePhases) {
+
+    std::vector<int> downstreamOrderedPhases;
     std::set<int> downstreamAvailablePhases;
-    for (auto myPhase : availablePhases) {
-        downstreamAvailablePhases.insert(myPhase);
-    };
 
     if (availablePhases.size() == 0) {       
-        for (auto orderedPhase : orderedPhases) {
-            std::cout << orderedPhase;
-        }
-        std::cout << std::endl;
+        printVector(orderedPhases);
+        return;
     } else{
-        for (auto phase : availablePhases) {   
-                orderedPhases.push_back(phase);
-                downstreamAvailablePhases.erase(phase);
-                everyCombination(orderedPhases, downstreamAvailablePhases, depth + 1);
-                //Next two lines help unwrap the recursive algorithm
-                orderedPhases.pop_back();  
-                downstreamAvailablePhases = availablePhases;
+        for (auto phase : availablePhases) { 
+            //Next two lines: Resets for every loop iteration    
+            downstreamOrderedPhases = orderedPhases;
+            downstreamAvailablePhases = availablePhases;
+            downstreamOrderedPhases.push_back(phase);
+            downstreamAvailablePhases.erase(phase);
+            everyCombination(downstreamOrderedPhases, downstreamAvailablePhases);
         }
-    }
-    return;
+        return;
+    }  
 }
 
 
 int main()
 {
-    int depth{ 1 };
     std::cout << "Running Proto...\n";
 
     std::set<int> puzzlePhases = { 0,1,2,3,4 };
     std::vector<int> orderedPhases;
 
-    everyCombination(orderedPhases, puzzlePhases, depth);
+    everyCombination(orderedPhases, puzzlePhases);
 
 }
 
