2e49242099dea0104785cf54919817c5b8a6b3b1
haselby <jeffrey.haselby@gmail.com>
2020-12-15 00:30:37 -0700

    Day7App_Part2 passing tests 1 and 2

--- 7/Day7App_Part2/Day7App_Part2/Day7App_Part2.cpp
+++ 7/Day7App_Part2/Day7App_Part2/Day7App_Part2.cpp
@@ -308,10 +308,11 @@ int main()
 	int maxThrust{ 0 };
 	
 	//TODO trying to get this test to pass
-	//TODO application compiles, but just sits as amp wait for input, that never comes.
+	
 	//text single configuration of phases
-	std::vector<int> testPuzzleInput = { 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5 };
-	std::vector<int> testOrderedPhases = { 9,8,7,6,5 };
+	//std::vector<int> testPuzzleInput = { 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5 }; //Passes
+	std::vector<int> testPuzzleInput = { 3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10 }; //Passes
+	std::vector<int> testOrderedPhases = { 9, 7, 8, 5, 6 };
 	testThruster(testPuzzleInput, testOrderedPhases);
 
 	//Disable line below temporarily
