935f846fa0495716480d6c83098c88a57c78ff62
haselby <jeffrey.haselby@gmail.com>
2020-12-15 00:40:32 -0700

    Day7App_Part2 added getter ampObject.getOutput(4)

--- 7/Day7App_Part2/Day7App_Part2/Day7App_Part2.cpp
+++ 7/Day7App_Part2/Day7App_Part2/Day7App_Part2.cpp
@@ -90,6 +90,10 @@ public:
 
 	};
 
+	int getOutput(int outputIndex) {
+		return ampOutput[outputIndex];
+	}
+
 
 	void compute(std::vector<int> intcodeProgram, long long instruction_pointer, int inputIndex, int outputIndex) {
 
@@ -271,7 +275,7 @@ int testThruster(std::vector<int>& puzzleInput, std::vector<int>& orderedPhases)
 	ampE.join();
 
 
-	return 4242;
+	return ampObject.getOutput(4);
 }
 
 void everyCombination(std::vector<int>& puzzleInput, std::vector<int> orderedPhases, std::set<int> availablePhases, int& maxThrust) {
@@ -313,7 +317,7 @@ int main()
 	//std::vector<int> testPuzzleInput = { 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5 }; //Passes
 	std::vector<int> testPuzzleInput = { 3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10 }; //Passes
 	std::vector<int> testOrderedPhases = { 9, 7, 8, 5, 6 };
-	testThruster(testPuzzleInput, testOrderedPhases);
+	maxThrust = testThruster(testPuzzleInput, testOrderedPhases);
 
 	//Disable line below temporarily
 	//everyCombination(puzzleInput, orderedPhases, puzzlePhases, maxThrust);
