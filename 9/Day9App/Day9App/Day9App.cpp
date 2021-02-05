303dd509aeff8fef29d15c1f58c8358046ff7454
haselby <jeffrey.haselby@gmail.com>
2021-02-05 16:27:24 -0700

    Minor code tweaks prior to adding Day 9 Functionality

--- 9/Day9App/Day9App/Day9App.cpp
+++ 9/Day9App/Day9App/Day9App.cpp
@@ -4,26 +4,33 @@
 #include <iostream>
 #include <vector>
 #include <set>
-#include <concrt.h> //Windows Only
-#include<vector>
+//Multithreading
+#include <concrt.h>
 #include<mutex>
-#include <boost/log/trivial.hpp> // Logging Framework
+// Logging Framework
+#include <boost/log/trivial.hpp>
 #include <boost/log/utility/setup/file.hpp>
 #include <boost/log/expressions.hpp>
-
+// Sleep
 using namespace std::this_thread;     // sleep_for, sleep_until
 using namespace std::chrono_literals; // ns, us, ms, s, h, etc.
 
 
-//Logging Framework
-// Added C:\devtools\boost_1_75_0\stage\lib folder to:
-// C++ Linker - Additional Library Directories
-// Logger Level Hierachy (ordered with most verbose at top)
-// trace
-// debug
-// info
-// warning
-// fatal
+/* Boost Logging Framework Integration
+ * 
+ * C/C++ - Additional Include Directories:
+ *     C:\devtools\boost_1_75_0
+ * Linker - Additional Library Directories:
+ *   C:\devtools\boost_1_75_0
+ *   C:\devtools\boost_1_75_0\stage\lib folder to:
+ *
+ * Logger Level Hierachy (ordered with most verbose at top)
+ * trace
+ * debug
+ * info
+ * warning
+ * fatal
+ */
 
 void init_logging()
 {
@@ -35,16 +42,19 @@ void init_logging()
 	);
 }
 
-struct InstructionDecoder {
-	InstructionDecoder(int myInstruction) {
-		instruction = myInstruction;
+class InstructionDecoder {
+	int instruction;
+
+public:
+	InstructionDecoder(int instruction) : instruction{ instruction } {
 		set_opcode();
 		set_num_parameters();
 		set_parameter_mode();
 	};
-	int instruction;
+
 	int opcode;
 	int num_parameters;
+
 	bool parameter1_isImmediate;
 	bool parameter2_isImmediate;
 	bool parameter3_isImmediate;
@@ -56,6 +66,7 @@ struct InstructionDecoder {
 	void set_opcode() {
 		opcode = (getDigit(instruction, 2) * 10) + getDigit(instruction, 1);
 	}
+
 	void set_num_parameters() {
 		if (opcode == 1) num_parameters = 3;
 		else if (opcode == 2) num_parameters = 3;
@@ -81,7 +92,6 @@ class Amp {
 
 public:
 	Amp(std::vector<int>& orderedPhases) {
-		//TODO: (Before leaving: Read in phase setting for all 5 amps in constructor)
 
 		ampOutput[0] = 0; // ampA output
 		ampOutput[1] = 0; // ampB output
@@ -323,20 +333,23 @@ int main()
 {
 	init_logging();
 	
+	// My Puzzle Input from AdventOfCode
 	std::vector<int> puzzleInput = { 3,8,1001,8,10,8,105,1,0,0,21,42,67,88,101,114,195,276,357,438,99999,3,9,101,3,9,9,1002,9,4,9,1001,9,5,9,102,4,9,9,4,9,99,3,9,1001,9,3,9,1002,9,2,9,101,2,9,9,102,2,9,9,1001,9,5,9,4,9,99,3,9,102,4,9,9,1001,9,3,9,102,4,9,9,101,4,9,9,4,9,99,3,9,101,2,9,9,1002,9,3,9,4,9,99,3,9,101,4,9,9,1002,9,5,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,1,9,9,4,9,99,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,2,9,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,102,2,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,1,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,101,1,9,9,4,9,3,9,1001,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1001,9,1,9,4,9,99,3,9,1001,9,2,9,4,9,3,9,102,2,9,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,1002,9,2,9,4,9,3,9,101,2,9,9,4,9,3,9,101,2,9,9,4,9,99 };
 
-	std::vector<int> orderedPhases;
+	std::vector<int> orderedPhases; // Initally empty, built up from puzzlePhases
 	std::set<int> puzzlePhases = { 5,6,7,8,9 };
 	int maxThrust{ 0 };
 
 
-	//text single configuration of phases
+	// Start: Test a single run
 	//std::vector<int> testPuzzleInput = { 3, 26, 1001, 26, -4, 26, 3, 27, 1002, 27, 2, 27, 1, 27, 26, 27, 4, 27, 1001, 28, -1, 28, 1005, 28, 6, 99, 0, 0, 5 }; //Passes
 	//std::vector<int> testPuzzleInput = { 3, 52, 1001, 52, -5, 52, 3, 53, 1, 52, 56, 54, 1007, 54, 5, 55, 1005, 55, 26, 1001, 54, -5, 54, 1105, 1, 12, 1, 53, 54, 53, 1008, 54, 0, 55, 1001, 55, 1, 55, 2, 53, 55, 53, 4, 53, 1001, 56, -1, 56, 1005, 56, 6, 99, 0, 0, 0, 0, 10 }; //Passes
 	//std::vector<int> testOrderedPhases = { 9, 7, 8, 5, 6 };
 	//maxThrust = testThruster(testPuzzleInput, testOrderedPhases);
+	// End: Test a single run 
 
 
+	// Run Every Permutation of puzzle phases, looking for the one that creates maxThrust.
 	everyCombination(puzzleInput, orderedPhases, puzzlePhases, maxThrust);
 
 	std::cout << "Max Thrust: " << maxThrust << std::endl;
